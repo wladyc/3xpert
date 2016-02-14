35610030a5339a30c4071d343bf0298cfaa62797
Wladimiro Carapucci <wlady@nemo.(none)>
2016-02-14 18:00:39 +0100

    Seconda versione object oriented

--- 3xpert.py
+++ 3xpert.py
@@ -1,175 +1,218 @@
-#!/usr/bin/python
-
-# print "Content-Type: text/plain"
-# print
-# print "Hello, 3xpert!"
-
 # Utilizzo la libreria logging per tracciare su un file di log
 try:
     import logging
-    logging.basicConfig(filename='3xpert.log',level=logging.INFO)
-    logging.info('Module logging imported')
+    logging.basicConfig(filename='test_3xpert.log',level=logging.INFO)
+    logging.info('module logging imported')
 except ImportError:
-    print 'Che sfortuna, non possiamo tracciare nulla...'
-# Utilizzo la libreria Requests: HTTP for Humans 
-try:
-    import requests
-    logging.info('Module requests imported')
-except ImportError:
-    logging.error('ImportError: No module named requests')
-# Utilizzo la libreria RE, che implementa le espressioni regolari in Python
-try:
-    import re
-    logging.info('Module re imported')
-except ImportError:
-    logging.error('ImportError: No module named re')
-import re
+    print 'che sfortuna, non possiamo tracciare nulla...'
+
 # Utilizzo il modulo sys per cambiare la codifica di default
 try:
     import sys
-    logging.info('Module sys imported')
+    logging.info('module sys imported')
 except ImportError:
-    logging.error('ImportError: No module named sys')
+    logging.error('error: no module named sys imported')
+
+# Utilizzo la libreria RE, che implementa le espressioni regolari in Python
+try:
+    import re
+    logging.info('module re imported')
+except ImportError:
+    logging.error('error: no module named re imported')
+
 # Import smtplib for the actual email sending function
 try:
     import smtplib
-    logging.info('Module smtplib imported')
+    logging.info('module smtplib imported')
 except ImportError:
-    logging.error('ImportError: No module named smtplib')
+    logging.error('error: no module named smtplib imported')
+
+# Utilizzo la libreria Requests: HTTP for Humans 
+try:
+    import requests
+    logging.info('module requests imported')
+except ImportError:
+    logging.error('error: no module named requests imported')
+
 # Import sleep e randint per recuperare la pagina 3xpert con ritardi casuali all'interno di un intervallo
 try:
     from time import sleep
     from random import randint
-    logging.info('Functions sleep and randint imported')
+    logging.info('functions sleep and randint imported')
 except ImportError:
-    logging.error('ImportError: No module named time or random')
+    logging.error('error: no functions named time or random imported')
+
 # Import datetime for timestamping
 try:
     import datetime
-    logging.info('Module datetime imported')
+    logging.info('module datetime imported')
 except ImportError:
-    logging.error('ImportError: No module named datetime')
+    logging.error('error: no module named datetime imported')
 
+class XpertEmailTo:
+    to = ''
+    def __init__(self, to):
+        self.to = to
 
+class XpertEmailServer:
+    emailLoggedIn = False
+    def __init__(self, smtpserver, port, sender_usr, sender_pwd):
+        try:
+            self.smtpserver = smtplib.SMTP_SSL(smtpserver, port)
+            self.smtpserver.ehlo()
+            self.smtpserver.ehlo
+            self.sender_usr = sender_usr
+            self.sender_pwd = sender_pwd
+            self.smtpserver.login(sender_usr, sender_pwd)
+            self.emailLoggedIn = True
+            logging.info('email logged in')
+        except:
+            self.emailLoggedIn = False
+            logging.info('email not logged in')
+    def xpertSendEmail(self, receiver, msg):
+        try:
+            logging.info(self.sender_usr)
+            logging.info(receiver)
+            self.smtpserver.sendmail(self.sender_usr, receiver, msg)
+            logging.info('mail sent')
+        except:
+            logging.error('mail problem')
+            self.emailLoggedIn = False
+    def __del__(self):
+        if self.smtpserver:
+            self.smtpserver.close()
+            logging.info('mail closed')
 
-# ==========================================================================
-# 1 GET
-# ==========================================================================
-# url_sign_in e' la pagina di login
-url_sign_in = 'https://social.tre.it/expert/sign_in'
-# url_expert_home e' l'home page che si richiede dopo il login e che
-# contiene eventualmente la domanda
-url_expert_home = 'https://social.tre.it/expert'
+class XpertUser:
+    login = ''
+    pwd = ''
+    def __init__(self, login, pwd):
+        self.login = login
+        self.pwd = pwd
 
-# Creo un oggetto session per avere la persistenza dei parametri (per esempio
-# dei cookie) attraverso una richiesta e le successive
-s = requests.session()
-logging.info('Persistent session created')
-
-# Utilizziamo il metodo di richiesta GET della sessione per
-# recuperare la pagina di login.
-# Ignoriamo la verifica del certificato SSL. Il valore di default e' True,
-# ma in questo caso dovremmo specificare una lista di CA di fiducia,
-# pena il lancio dell'eccezione requests.exceptions.SSLError
-# Specifichiamo anche uno User-agent
-r1 = s.get(url_sign_in, verify = False, headers = {'User-agent': 'Mozilla/5.0'})
-logging.info('GET for sign_in page')
-
-# ==========================================================================
-# 2 POST
-# ==========================================================================
-# Prepariamo il payload del metodo POST con il quale passiamo i parametri
-# necessari all'autenticazione. Oltre alle credenziali di accesso dobbiamo
-# impostare anche il parametro terms_of_service, altrimenti il server
-# non ci fa accedere. Per conoscere i nomi che devono essere usati
-# sfruttiamo i Developer tools di Chrome.
-# Nel metodo post rimandiamo, inoltre, il cookie di sessione: il nome
-# utilizzato per esso e' _session_id (Developer Tools di Chrome).
-# E' anche necessario mostrare al server che non e' in corso un
-# attacco di tipo CSRF, impostando il paramentro authenticity_token
-# del metodo POST con il valore letto dalla precedente GET del
-# token CSRF.
-
-# Attraverso il metodo search del modulo re, cerchiamo nella pagina
-# di login ottenuta con il comando GET l'espressione regolare
-# 'meta content="(.*)" name="csrf-token" /'
-
-# Spostare sopra
-# Per prima cosa dobbiamo modificare la codifica di default da ascii
-# a utf-8 (http://mypy.pythonblogs.com/12_mypy/archive/1253_workaround_for_python_bug_ascii_codec_cant_encode_character_uxa0_in_position_111_ordinal_not_in_range128.html)
-# Istruzione di debug
-# sys.getdefaultencoding()
-reload(sys);
-sys.setdefaultencoding("utf8")
-matchme = 'meta content="(.*)" name="csrf-token" /' 
-csrf = re.search(matchme, str(r1.text))
-
-# Istruzione di debug
-# print csrf.group(1)
-# payload = {'expert[email]': 'wladimiro.carapucci@gmail.com', 'expert[password]': '3Xpert_Tr3', 'expert[remember_me]': '0', 'expert[terms_of_service]': '1', 'authenticity_token' : csrf.group(1), '_session_id': r1.cookies["_session_id"]}
-payload = {'expert[email]': 'wladimiro.carapucci@gmail.com', 'expert[password]': '3Xpert_Tr3', 'expert[remember_me]': '0', 'expert[terms_of_service]': '1', 'authenticity_token' : csrf.group(1)}
-
-r2 = s.post(url_sign_in, data = payload, verify = False)
-logging.info('POST for sign_in page')
-
-# Istruzione di debug
-# print r2.content
-
-
-# ==========================================================================
-# 3 HANDLER FOR LE TUE DOMANDE
-# ==========================================================================
-to = 'wladimiro.carapucci@gmail.com'
-
-gmail_user = 'carapucci.bimby@gmail.com'
-gmail_pwd = 'bimbymio'
-domanda_ok = 'ecco la tua domanda!'
-domanda_ko = 'non ci sono domande per te'
-matchme2 = '@Wladimiro Carapucci, (.*)</h1><div class="switcher'
-
-
-while 1:
-    # Ritardo casuale in un intervallo espresso in secondi
-    sleep(randint(60,120))
-    logging.info(datetime.datetime.now())
-
-    smtpserver = smtplib.SMTP_SSL("smtp.gmail.com",465)
-    smtpserver.ehlo()
-    smtpserver.ehlo
-    smtpserver.login(gmail_user, gmail_pwd)
-
-    r1 = s.get(url_expert_home, verify = False, headers = {'User-agent': 'Mozilla/5.0'})    
-    domanda_per_te = re.search(matchme2, str(r2.text))
-
-
-    if domanda_per_te.group(1):
-    # la stringa e' stata trovata
-        if (domanda_per_te.group(1) == domanda_ok):
-        # sendmail con domanda nell'oggetto
-            header = 'To: ' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject: 3xpert, domanda per te\n'
-            msg = header + '\n 3xpert, ecco la domanda per te \n\n'
-            smtpserver.sendmail(gmail_user, to, msg)
-            logging.info('Mail sent')
-        elif (domanda_per_te.group(1) == domanda_ko):
-        # incrementare un contatore? e' tutto ok, ma si puo' mandare una e-mail che lo conferma
-            header = 'To: ' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject: 3xpert, nessuna anomalia \n'
-            msg = header + '\n 3xpert, non ci sono ancora domande per te \n\n'
-            smtpserver.sendmail(gmail_user, to, msg)
-            logging.info('Mail sent')
+class XpertPortal:
+    xpertLoggedIn = False
+    counter_domanda_ko = 0
+    def __init__(self, sign_in_url, home_url):
+        self.sign_in_url = sign_in_url
+        self.home_url = home_url
+        self.s = requests.session()
+        reload(sys);
+        sys.setdefaultencoding("utf8")
+    def xpertScraping(self, matchme, url):
+        webPage = self.s.get(url, verify = False, headers = {'User-agent': 'Mozilla/5.0'})        
+        return re.search(matchme, str(webPage.text))
+    def xpertLogin(self, payload):
+        try:
+            self.s.post(self.sign_in_url, payload, verify = False)
+            self.xpertLoggedIn = True
+            logging.info('3xpert logged in')
+        except:
+            xpertLoggedIn = False
+            logging.info('3xpert not logged in')
+    def xpertDomanda(self, domanda, emailTo, emailServer):
+        domanda_ok = 'la tua domanda!'
+        domanda_ko = 'non ci sono domande per te'
+        # logging.info('Domanda')
+        header = 'To: ' + emailTo + '\n' + 'From: ' + emailServer.sender_usr + '\n' + 'Subject: 3xpert\n'
+        if (domanda):
+            if (domanda.group(1) == domanda_ok):
+                msg = header + '\n 3xpert, ' + domanda_ok + '\n\n'
+                logging.info(emailServer)
+                logging.info(emailTo)
+                emailServer.xpertSendEmail(emailTo, msg)
+                logging.info(domanda_ok)
+            elif (domanda.group(1) == domanda_ko):
+                logging.info(domanda_ko)
+                if (self.counter_domanda_ko == 4):
+                    msg = header + '\n 3xpert, ' + domanda_ko + '\n\n'
+                    logging.info(emailServer)
+                    logging.info(emailTo)
+                    emailServer.xpertSendEmail(emailTo, msg)
+                    logging.info(domanda_ko)
+                    self.counter_domanda_ko = 0
+                else:
+                    self.counter_domanda_ko += 1
         else:
-        # anomalia; sendmail con oggetto anomalia e body domanda_per_te
-            header = 'To: ' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject: 3xpert, anomalia \n'
-            msg = header + '\n 3xpert, stringa inattesa\n\n'
-            smtpserver.sendmail(gmail_user, to, msg)
-            logging.info('Mail sent')
+            msg = header + '\n 3xpert, homepage error ' + '\n\n'
+            emailServer.xpertSendEmail(emailTo, msg)
+            logging.error('homepage error')
+#    def __del__(self):
+#        if self.s:
+#            self.s.close()
+#            logging.info('Session closed')
+
+def setupXpert(xpertEmailTo, xpertEmailServer, xpertUser, xpertPortal):
+    try:
+        header = 'To: ' + xpertEmailTo + '\n' + 'From: ' + xpertEmailServer.sender_usr + '\n' + 'Subject: 3xpert\n'
+        msg = header + '\n email framework ready to be used \n\n'
+        xpertEmailServer.xpertSendEmail(xpertEmailTo, msg)
+    except:
+        logging.error('email framework error')
+
+    try:
+        csrf = xpertPortal.xpertScraping('meta content="(.*)" name="csrf-token" /', xpertPortal.sign_in_url)
+        logging.info(csrf.group(1))
+        data = {'expert[email]': xpertUser.login, 'expert[password]': xpertUser.pwd, 'expert[remember_me]': '1', 'expert[terms_of_service]': '1', 'authenticity_token' : csrf.group(1)}
+        xpertPortal.xpertLogin(data)
+        logging.info('3xpert portal logged in')
+    except:
+        logging.error('3xpert portal error')
+        header = 'To: ' + xpertEmailTo + '\n' + 'From: ' + xpertEmailServer.sender_usr + '\n' + 'Subject: 3xpert\n'
+        msg = header + '\n 3xpert portal error \n\n'
+        xpertEmailServer.xpertSendEmail(xpertEmailTo, msg)
+
+    return {'emailLoggedIn':xpertEmailServer.emailLoggedIn, 'xpertPortalLoggedIn':xpertPortal.xpertLoggedIn}
+
+if __name__ == "__main__":
+    xpertEmailTo = XpertEmailTo('wladimiro.carapucci@gmail.com')
+    xpertEmailServer = XpertEmailServer('smtp.gmail.com', 465, 'carapucci.bimby@gmail.com', 'bimbymio')
+    xpertUser = XpertUser('wladimiro.carapucci@gmail.com', '3Xpert_3')
+    xpertPortal = XpertPortal('https://social.tre.it/expert/sign_in', 'https://social.tre.it/expert')
+    login = setupXpert(xpertEmailTo.to, xpertEmailServer, xpertUser, xpertPortal)
+    if ((login['emailLoggedIn'] == True) and (login['xpertPortalLoggedIn'] == True)):
+        while True:
+            if (datetime.datetime.now().hour == 0):
+                logging.info('3xpert, good night')
+                sleep(8*60*60)
+            elif (datetime.datetime.now().hour == 1):
+                logging.info('3xpert, good night')
+                sleep(7*60*60)
+            elif (datetime.datetime.now().hour == 2):
+                logging.info('3xpert, good night')
+                sleep(6*60*60)
+            elif (datetime.datetime.now().hour == 3):
+                logging.info('3xpert, good night')
+                sleep(5*60*60)
+            elif (datetime.datetime.now().hour == 4):
+                logging.info('3xpert, good night')
+                sleep(4*60*60)
+            elif (datetime.datetime.now().hour == 5):
+                logging.info('3xpert, good night')
+                sleep(3*60*60)
+            elif (datetime.datetime.now().hour == 6):
+                logging.info('3xpert, good night')
+                sleep(2*60*60)
+            elif (datetime.datetime.now().hour == 7):
+                logging.info('3xpert, good night')
+                sleep(1*60*60)
+            else:
+                logging.info(datetime.datetime.now())
+                domanda_per_te = xpertPortal.xpertScraping('@Wladimiro Carapucci, (.*)</h1><div class="switcher', xpertPortal.home_url)
+                if (xpertEmailServer.emailLoggedIn == False):
+                    logging.info('email user not logged in')
+                    xpertEmailServer = XpertEmailServer('smtp.gmail.com', 465, 'carapucci.bimby@gmail.com', 'bimbymio')
+                    header = 'To: ' + xpertEmailTo.to + '\n' + 'From: ' + xpertEmailServer.sender_usr + '\n' + 'Subject: 3xpert\n'
+                    msg = header + '\n email server created again\n\n'
+                    xpertEmailServer.xpertSendEmail(xpertEmailTo.to, msg)
+                    logging.info('email server created again')
+                xpertPortal.xpertDomanda(domanda_per_te, xpertEmailTo.to, xpertEmailServer)
+                # Random delay
+                sleep(randint(300,330))
     else:
-    # anomalia; sendmail con oggetto anomalia e body "Non ho trovato la stringa "
-        header = 'To: ' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject: 3xpert, anomalia \n'
-        msg = header + '\n 3xpert, non ho trovato la stringa\n\n'
-        smtpserver.sendmail(gmail_user, to, msg)
-        logging.info('Mail sent')
-
-    # print 'done!'
-    smtpserver.close()
-    logging.info('Mail server disconnected')
-
+        logging.error('connections to be closed')
+        header = 'To: ' + xpertEmailTo.to + '\n' + 'From: ' + xpertEmailServer.sender_usr + '\n' + 'Subject: 3xpert\n'
+        msg = header + '\n 3xpert, setup error... exit\n\n'
+        xpertEmailServer.xpertSendEmail(xpertEmailTo.to, msg)
+        del xpertEmailServer
+#        del xpertPortal
